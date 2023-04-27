39ce774e6e48933f892916e851523c6103641b69
CetusWhale <yi-xuan_yu24@milton.edu>
2023-04-27 15:16:14 -0400

    update

--- src/WikiGame.java
+++ src/WikiGame.java
@@ -6,19 +6,35 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.util.ArrayList;
 
-public class WikiGame {
+public class WikiGame implements ActionListener {
 
+    private JFrame mainFrame;
+    private JLabel headerLabel;
+    private JTextArea statusLabel;
+    private JPanel controlPanel;
+    private JMenuBar mb;
+    private JMenu file, edit, help;
+    private JMenuItem cut, copy, paste, selectAll;
+    private JTextArea ta;
+    private JTextArea tb;
+    private int WIDTH = 800;
+    private int HEIGHT = 700;
+    private JScrollPane scrollPane;
+    private JScrollPane scrollPane1;
+    private JScrollPane scrollPane2;
     private int maxDepth;
     private ArrayList<String> path = new ArrayList<>();
 
     public static void main(String[] args) {
         WikiGame w = new WikiGame();
+
+          w.showEventDemo();
     }
 
     public WikiGame() {
-
-        String startLink = "";  // beginning link, where the program will start
-        String endLink = "";    // ending link, where the program is trying to get to
+        SwingControlDemo();
+        String startLink = "https://en.wikipedia.org/wiki/Car";  // beginning link, where the program will start
+        String endLink = "https://en.wikipedia.org/wiki/Truck";    // ending link, where the program is trying to get to
         maxDepth = 1;           // start this at 1 or 2, and if you get it going fast, increase
 
         if (findLink(startLink, endLink, 0)) {
@@ -36,48 +52,80 @@ public class WikiGame {
         System.out.println("depth is: " + depth + ", link is: https://en.wikipedia.org" + startLink);
 
         // BASE CASE
-        if () {
-
-        } else if () {
-
+        if (endLink.equals(startLink)) {
+            System.out.println("found it");
+        } else if (depth==maxDepth) {
+            System.out.println("cannot find it");
         }
 
         // GENERAL RECURSIVE CASE
         else {
+            readhtml(startLink);
 
         }
 
         return false;
     }
+    public void readhtml(String start) {
+        try {
+            System.out.println();
+            System.out.print("hello \n");
+            URL url = new URL(start);
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(url.openStream())
+            );
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println("og: " + line);
 
-    class Wikigame implements ActionListener {
-        private JFrame mainFrame;
-        private JLabel headerLabel;
-        private JTextArea statusLabel;
-        private JPanel controlPanel;
-        private JMenuBar mb;
-        private JMenu file, edit, help;
-        private JMenuItem cut, copy, paste, selectAll;
-        private JTextArea ta;
-        private JTextArea tb;
-        private int WIDTH = 800;
-        private int HEIGHT = 700;
-        private JScrollPane scrollPane;
-        private JScrollPane scrollPane1;
-        private JScrollPane scrollPane2;
+                if (line.contains("href") && line.contains(tb.getText())) {
+                    System.out.println("og: " + line);
+
+                    int index = line.indexOf("href");
+                    line = line.substring(index + 6);
+                    if (line.contains("http")) {
+                        index = line.indexOf("http");
+                        line = line.substring(index);
+                        int endD = line.indexOf("\"");
+                        int endS = line.indexOf("\'");
+                        int end = 0;
+                        if (endD == -1) {
+                            end = endS;
+                        }
+                        if (endS == -1) {
+                            end = endD;
+                        }
+                        if (endD != -1 && endS != -1) {
+                            if (endD > endS) {
+                                end = endS;
+                            } else {
+                                end = endD;
+
+                            }
+                        }
+                    }
+                }
+            }
+        }catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+
+  //  class Wikigame implements ActionListener {
+
 
 
         public void SwingControlDemo() {
             prepareGUI();
         }
 
-        public static void main(String[] args) {
-            //SwingControlDemo swingControlDemo = new SwingControlDemo();
-            // swingControlDemo.showEventDemo();
-            Wikigame h = new Wikigame();
-            h.SwingControlDemo();
-            h.showEventDemo();
-        }
+//        public static void main(String[] args) {
+//            //SwingControlDemo swingControlDemo = new SwingControlDemo();
+//            // swingControlDemo.showEventDemo();
+//            Wikigame h = new Wikigame();
+//            h.SwingControlDemo();
+//            h.showEventDemo();
+//        }
 
         private void prepareGUI() {
             mainFrame = new JFrame("Java SWING Examples");
@@ -106,9 +154,9 @@ public class WikiGame {
             mb.add(edit);
             mb.add(help);
 
-            ta = new JTextArea();
+            ta = new JTextArea("start");
             ta.setBounds(50, 5, WIDTH - 100, HEIGHT - 50);
-            tb = new JTextArea();
+            tb = new JTextArea("end");
             tb.setBounds(50, 5, WIDTH - 100, HEIGHT - 50);
             mainFrame.add(mb);
             mainFrame.setJMenuBar(mb);
@@ -179,54 +227,7 @@ public class WikiGame {
 
                 if (command.equals("OK")) {
                     statusLabel.setText("Ok Button clicked.\n");
-                    try {
-                        System.out.println();
-                        System.out.print("hello \n");
-                        URL url = new URL(ta.getText());
-                        BufferedReader reader = new BufferedReader(
-                                new InputStreamReader(url.openStream())
-                        );
-                        String line;
-                        while ((line = reader.readLine()) != null) {
-                            System.out.println("og: " + line);
 
-                            if (line.contains("href") && line.contains(tb.getText())) {
-                                System.out.println("og: " + line);
-
-                                int index = line.indexOf("href");
-                                line = line.substring(index + 6);
-                                if (line.contains("http")) {
-                                    index = line.indexOf("http");
-                                    line = line.substring(index);
-                                    int endD = line.indexOf("\"");
-                                    int endS = line.indexOf("\'");
-                                    int end = 0;
-                                    if (endD == -1) {
-                                        end = endS;
-                                    }
-                                    if (endS == -1) {
-                                        end = endD;
-                                    }
-                                    if (endD != -1 && endS != -1) {
-                                        if (endD > endS) {
-                                            end = endS;
-                                        } else {
-                                            end = endD;
-
-                                        }
-                                    }
-
-
-                                    System.out.println(line.substring(0, end));
-                                    statusLabel.append(line.substring(0, end) + "\n");
-                                }
-                            }
-
-                        }
-                        reader.close();
-                    } catch (Exception ex) {
-                        System.out.println(ex);
-                    }
 
 
                 } else if (command.equals("Submit")) {
@@ -236,5 +237,5 @@ public class WikiGame {
                 }
             }
         }
-    }
+   // }
 }
